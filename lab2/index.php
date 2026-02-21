a8d7ae32811abeea9a591b30e859146ffc3d8a01
Aloooo <_email@example.com>
2026-02-21 12:27:24 +0200

    Lab2

---
+++ lab2/index.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Hello, world!";
+echo "<br>Hello, World with echo!";
+print "<br>Hello, World with print!";
+
+$days = 288;
+
+$message = "Все возвращаются на работу!";
+
+echo "<br>moya statia " . $days;
+
+echo "<br>Very sad that $message";
+?>
